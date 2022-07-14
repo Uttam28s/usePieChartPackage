2a8b50ee894ba8a7f5ec3fe7ea7a87f79e331f4f
Uttam28s <umoradiya31@gmail.com>
2022-07-14 23:44:59 +0530

    First commit with piechart pakage

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { SimplePieChart } from "piechartpackage";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <SimplePieChart
+          data={[
+            { title: "One", value: 10, color: "#E38627" },
+            { title: "Two", value: 15, color: "#C13C37" },
+            { title: "Three", value: 20, color: "#6A2135" },
+            { title: "Four", value: 45, color: "#6A2835" },
+          ]}
+          radius={50}
+        />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
